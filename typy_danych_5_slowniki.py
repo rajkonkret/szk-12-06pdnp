3e561757bfa1adb427fe5da76436621e49b57c18
rajkonkret <52714027+rajkonkret@users.noreply.github.com>
2023-06-13 09:41:22 +0200

    update slowniki

--- typy_danych_5_slowniki.py
+++ typy_danych_5_slowniki.py
@@ -19,3 +19,16 @@ dictionary = {'x': 2}
 print(dictionary)
 dictionary.update([('y', 3), ('z', 0)])
 print(dictionary)  # {'x': 2, 'y': 3, 'z': 0}
+
+dict2 = {'imie': 'name', 'kot': 'cat'}
+print(dict2['kot'])
+# dict2.update({'pies': "dog"})  # dodawanie elementu do słownika
+# print("Mamy w słowniku", dict2.keys())
+# key = input("Podaj słowo do przetłumaczenia")  # input zwraca str
+# print(key)
+# print(type(key))  # <class 'str'>
+# print(dict2[key.lower().replace(" ", "")])
+
+a = int(input("Podaj pierwsza liczbe"))
+b = int(input("Podaj drugą liczbe"))
+print(a + b)
